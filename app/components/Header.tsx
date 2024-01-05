f07e9031e47b406713e0ddeb5fdf9a097731173d
aolyang <aolyang@foxmail.com>
2024-01-06 00:23:31 +0800

    chore(pkg): install packages

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,35 @@
+import "@/styles/header.scss"
+
+import { Link, useLocation } from "@remix-run/react"
+import { useTranslation } from "react-i18next"
+
+import { clsx } from "clsx"
+import { type ReactNode } from "react"
+
 import { site } from "@/site"
 
-export default function Header({ children }: { children: React.ReactNode }) {
+const navs = ["posts", "tags", "categories", "about"]
+export default function Header({ children }: { children: ReactNode }) {
+    const { t } = useTranslation()
+    const location = useLocation()
     return (
         <>
-            <header className={"p-4 w-full flex justify-center"}>
+            <header className={"nav-header p-4 w-full flex justify-center"}>
                 <div className={"w-[48rem] flex justify-between"}>
-                    <h1 className={"text-2xl"}>{site.title}</h1>
+                    <h1 className={"text-2xl"}>
+                        <Link to={"/"}>{site.title}</Link>
+                    </h1>
                     <ul className={"flex gap-4"}>
-                        <li>Posts</li>
-                        <li>Categories</li>
-                        <li>About</li>
+                        {navs.map((nav) => (
+                            <li
+                                key={nav}
+                                className={clsx({
+                                    active: location.pathname.startsWith(`/${nav}`)
+                                })}
+                            >
+                                <Link to={`/${nav}`}>{t(nav)}</Link>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </header>
