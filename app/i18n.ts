d8f5a23f4573a109ef185bb2a9ee91934d1cfe0e
aolyang <aolyang@foxmail.com>
2024-01-06 22:43:03 +0800

    chore(i18n): add type

--- app/i18n.ts
+++ app/i18n.ts
@@ -1,6 +1,8 @@
+import { type InitOptions } from "i18next"
+
 export default {
     supportedLngs: ["en", "zh"],
     fallbackLng: "en",
     defaultNS: "common",
     react: { useSuspense: false }
-}
+} satisfies InitOptions
