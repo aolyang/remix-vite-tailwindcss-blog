f6c8b5ae3037b21ca974d9d1ee205a41cd1d3f68
aolyang <aolyang@foxmail.com>
2024-01-08 00:19:25 +0800

    chore(site): just remove space

--- app/routes/[sitemap.xml].tsx
+++ app/routes/[sitemap.xml].tsx
@@ -2,7 +2,7 @@ import { site } from "@/site"
 
 export const loader = async () => {
     const content = `
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset xmlns="https://sitemaps.org/schemas/sitemap/0.9/">
         <url>
             <loc>${site.website}</loc>
             <lastmod>${site.lastModTime}</lastmod>
@@ -10,6 +10,9 @@ export const loader = async () => {
         </url>
     </urlset>
     `
+        .replace(/^\n/, "")
+        .replace(/^ +/gm, "")
+
     // Return the response with the content, a status 200 message, and the appropriate headers for an XML page
     return new Response(content, {
         status: 200,
